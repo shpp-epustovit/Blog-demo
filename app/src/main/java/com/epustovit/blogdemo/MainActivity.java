b61d5f39320cbce6978c787a2701933132e5485d
shpp-epustovit <smithpev@gmail.com>
2017-10-04 17:05:12 +0300

    add menu

--- app/src/main/java/com/epustovit/blogdemo/MainActivity.java
+++ app/src/main/java/com/epustovit/blogdemo/MainActivity.java
@@ -1,15 +1,130 @@
 package com.epustovit.blogdemo;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser mUser;
+
+    private Button loginButton;
+    private Button createActButton;
+    private EditText emailField;
+    private EditText passwordField;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button)findViewById(R.id.loginButtonEt);
+        createActButton = (Button)findViewById(R.id.loginCreateAccount);
+        emailField = (EditText)findViewById(R.id.loginEmaiEt);
+        passwordField = (EditText)findViewById(R.id.loginPasswordEt);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                mUser = firebaseAuth.getCurrentUser();
+                if (mUser != null) {
+                    // User is signed in
+                    Toast.makeText(MainActivity.this, "Signed in", Toast.LENGTH_SHORT).show();
+                } else {
+                    // User is signed out
+                    Toast.makeText(MainActivity.this, "Not signet in", Toast.LENGTH_SHORT).show();
+                }
+                // ...
+            }
+        };
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!TextUtils.isEmpty(emailField.getText().toString()) && !TextUtils.isEmpty(passwordField.getText().toString())){
+
+                    String email = emailField.getText().toString();
+                    String pwd = passwordField.getText().toString();
+
+                    login(email,pwd);
+
+                }else {
+
+
+                }
+            }
+        });
+    }
+
+    private void login(String email, String pwd) {
+
+        mAuth.signInWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            // We're in!
+                            Toast.makeText(MainActivity.this, "Signed in", Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(MainActivity.this, PostListActivity.class));
+
+                        }else{
+
+
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_signout){
+            mAuth.signOut();
+        }
+
+            return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
